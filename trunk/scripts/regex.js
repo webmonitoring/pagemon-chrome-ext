c8df252557d38850c201fb09b66dc9f1f6eccb02
max99x <max99x@20b4226c-1407-11df-9d2a-2365f6df44e6>
2010-05-19 13:50:40 +0000

    * Added a few simple tests for regex.js. * Added some details to the documentation of regex.js. * Made regex.js silently return rather than raise an error when passed invalid JSON.

--- trunk/scripts/regex.js
+++ trunk/scripts/regex.js
@@ -1,17 +1,24 @@
 /*
-  A Web Worker script to allow interruptable regex searches. Expects the command
-  in the following format:
+  A Web Worker script to allow interruptable regex searches. Expects a command
+  with the value in the following format (JSON-encoded as a string):
   {
     command: 'run',
     text: 'the text to search in',
     regex: 'the regex pattern string'
   }
-  Returns a string of all the matches, separated by new lines (there may also be
-  embedded new lines inside the matches).
+  Returns a string of all the matches, each between double quotes and separated
+  by new lines (there may also be embedded new lines inside the matches).
+  
+  NOTE: If called directly rather than through the Web Worker API, the command
+  object should be inside the "data" property of an enclosing object.
 */
 
 onmessage = function(raw_message) {
-  var message = JSON.parse(raw_message.data);
+  try {
+    var message = JSON.parse(raw_message.data);
+  } catch (e) {
+    return;
+  }
   
   if (message.command == 'run') {
     var text = message.text;
