3da18d77fa18e7e7f1139fe3b19b919432f326e9
Max Shawabkeh <max99x@users.noreply.github.com>
2010-05-25 21:37:45 +0000

    Fixed JSON strings in the regex test to be double-quoted following the standard, which is enforced on Chrome 5 but not Chrome 4.

--- tests/regex.js
+++ tests/regex.js
@@ -1,20 +1,36 @@
 $(function() {
   test('Invalid', function() {
-    postMessage = function() { ok(false, 'Called when it shouldn\'t.'); };
+    expect(0);
+    
+    postMessage = function() { ok(false, 'Empty string.'); };
     onmessage('');
+    
+    postMessage = function() { ok(false, '"Run" as a string.'); };
     onmessage('run');
+    
+    postMessage = function() { ok(false, 'Unserialized command.'); };
     onmessage({ command: 'go', text: 't', regex: 't' });
-    onmessage({ data: { command: 'go', text: 't', regex: 't' } });
-    onmessage({ data: "{ command: 'go', text: 't', regex: 't' }" });
+    
+    postMessage = function() { ok(false, 'Unserialized wrapped command.'); };
     onmessage({ data: { command: 'run', text: 't', regex: 't' } });
+    
+    postMessage = function() { ok(false, 'Invalid command string.'); };
+    onmessage({ data: '{ "command": "go", "text": "t", "regex": "t" }' });
   });
   
   test('Valid', function() {
     expect(3);
-    postMessage = function() { ok(true, 'Called when it should.'); };
-    onmessage({ data: "{ command: 'run', text: 't', regex: 't' }" });
-    onmessage({ data: "{ command: 'run' }" });
-    onmessage({ data: "{ command: 'run', other: 'test' }" });
+    
+    postMessage = function() { ok(true, 'Filled text and regex.'); };
+    onmessage({ data: '{ "command": "run", "text": "t", "regex": "t" }' });
+    
+    postMessage = function() { ok(true, 'Missing text and regex.'); };
+    onmessage({ data: '{ "command": "run" }' });
+    
+    postMessage = function() {
+      ok(true, 'Missing text and regex and an extra property.');
+    };
+    onmessage({ data: '{ "command": "run", "other": "test" }' });
   });
   
   test('Value Check', function() {
@@ -23,22 +39,22 @@ $(function() {
     postMessage = function(result) {
       equals(result, '"a"', 'Simple string match');
     };
-    onmessage({ data: "{ command: 'run', text: 'a', regex: 'a' }" });
+    onmessage({ data: '{ "command": "run", "text": "a", "regex": "a" }' });
     
     postMessage = function(result) {
       equals(result, '"a"', 'Simple regex match');
     };
-    onmessage({ data: "{ command: 'run', text: 'a', regex: '.' }" });
+    onmessage({ data: '{ "command": "run", "text": "a", "regex": "." }' });
     
     postMessage = function(result) {
       equals(result, '"a"\n"a"', 'Multiple string match');
     };
-    onmessage({ data: "{ command: 'run', text: 'aba', regex: 'a' }" });
+    onmessage({ data: '{ "command": "run", "text": "aba", "regex": "a" }' });
     
     postMessage = function(result) {
       equals(result, '"a"\n"b"\n"a"', 'Multiple regex match');
     };
-    onmessage({ data: "{ command: 'run', text: 'abca', regex: '[^c]' }" });
+    onmessage({ data: '{ "command": "run", "text": "abca", "regex": "[^c]" }' });
     
     postMessage = function(result) {
       equals(result, '"ab\nc"\n"d\ne"', 'Embedded newlines');
