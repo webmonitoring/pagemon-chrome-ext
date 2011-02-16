65ea05d4ca4dce5bbae23362e0f3a2635a204395
max99x <max99x@20b4226c-1407-11df-9d2a-2365f6df44e6>
2011-02-16 03:45:34 +0000

    Standardized build.py indentation to 2 spaces per level.

--- trunk/build.py
+++ trunk/build.py
@@ -5,93 +5,98 @@ import re
 import shutil
 import sys
 
+
 DEV_ONLY_REGEX = re.compile('<!--DEV_ONLY-->.*?<!--/DEV_ONLY-->', re.DOTALL)
 
+
 def compileJS(text):
-    import httplib, urllib, sys
+  import httplib, urllib, sys
 
-    params = urllib.urlencode([('js_code', text),
-                               ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
-                               ('output_format', 'text'),
-                               ('output_info', 'compiled_code')])
-    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+  params = urllib.urlencode([('js_code', text),
+                             ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
+                             ('output_format', 'text'),
+                             ('output_info', 'compiled_code')])
+  headers = {'Content-type': 'application/x-www-form-urlencoded'}
 
-    conn = httplib.HTTPConnection('closure-compiler.appspot.com')
-    conn.request('POST', '/compile', params, headers)
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
-    
-    return data.replace('<!--', r'\<\!\-\-')
+  conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+  conn.request('POST', '/compile', params, headers)
+  response = conn.getresponse()
+  data = response.read()
+  conn.close()
+
+  return data.replace('<!--', r'\<\!\-\-')
+
 
 def compileCSS(text):
-    return re.sub(r'/\*.*?\*/|(?<!\w)\s+|\s(?=\{)', '', text.strip())
+  return re.sub(r'/\*.*?\*/|(?<!\w)\s+|\s(?=\{)', '', text.strip())
 
 def compileHTML(filename):
-    string = open(filename).read()
-    string = DEV_ONLY_REGEX.sub('', string)
-    doc = lxml.html.fromstring(string)
-    
-    for e in doc.cssselect('*'):
-        if not e.tail or e.tail.isspace():
-            e.tail = ' '
+  string = open(filename).read()
+  string = DEV_ONLY_REGEX.sub('', string)
+  doc = lxml.html.fromstring(string)
 
-    for script in doc.cssselect('script'):
-        if script.text:
-            script.text = compileJS(script.text)
-    
-    for style in doc.cssselect('style'):
-        if style.text:
-            style.text = compileCSS(style.text)
-    
-    return lxml.html.tostring(doc)
+  for e in doc.cssselect('*'):
+    if not e.tail or e.tail.isspace():
+      e.tail = ' '
+
+  for script in doc.cssselect('script'):
+    if script.text:
+      script.text = compileJS(script.text)
+
+  for style in doc.cssselect('style'):
+    if style.text:
+      style.text = compileCSS(style.text)
+
+  return lxml.html.tostring(doc)
+
 
 def stripDotfiles(src, names):
-    return [i for i in names if i.startswith('.')]
+  return [i for i in names if i.startswith('.')]
+
 
 if __name__ == '__main__':
-    # Remove old build folder.
-    if os.path.exists('build'):
-        print 'Cleaning old folder'
-        shutil.rmtree('build')
-    
-    # Copy all files.
-    print 'Copying files'
-    shutil.copytree('.', 'build', ignore=stripDotfiles)
+  # Remove old build folder.
+  if os.path.exists('build'):
+    print 'Cleaning old folder'
+    shutil.rmtree('build')
 
-    # Copy over minified libs.
-    print 'Applying minified libs'
-    for f in glob.glob('build/lib/*.js'):
-        os.remove(f)
-    for f in glob.glob('build/lib/min/*.js'):
-        shutil.copy(f, 'build/lib')
-    shutil.rmtree('build/lib/min')
+  # Copy all files.
+  print 'Copying files'
+  shutil.copytree('.', 'build', ignore=stripDotfiles)
 
-    # Remove the build script.
-    print 'Removing build script'
-    os.remove('build/build.py')
+  # Copy over minified libs.
+  print 'Applying minified libs'
+  for f in glob.glob('build/lib/*.js'):
+    os.remove(f)
+  for f in glob.glob('build/lib/min/*.js'):
+    shutil.copy(f, 'build/lib')
+  shutil.rmtree('build/lib/min')
 
-    # Remove the tests.
-    print 'Removing tests'
-    shutil.rmtree('build/tests')
+  # Remove the build script.
+  print 'Removing build script'
+  os.remove('build/build.py')
 
-    # Compile stylesheets.
-    for f in glob.glob('build/styles/*.css'):
-        print 'Compiling CSS:', f
-        data = open(f).read()
-        open(f, 'w').write(compileCSS(data))
+  # Remove the tests.
+  print 'Removing tests'
+  shutil.rmtree('build/tests')
 
-    # Compile javascript.
-    if '--compile_js' in sys.argv:
-      for f in glob.glob('build/scripts/*.js'):
-          print 'Compiling JS:', f
-          data = open(f).read()
-          open(f, 'w').write(compileJS(data))
+  # Compile stylesheets.
+  for f in glob.glob('build/styles/*.css'):
+    print 'Compiling CSS:', f
+    data = open(f).read()
+    open(f, 'w').write(compileCSS(data))
 
-    # Compile HTML.
-    for f in glob.glob('build/*.htm*'):
-        print 'Compiling HTML:', f
-        data = compileHTML(f)
-        open(f, 'w').write(data)
+  # Compile javascript.
+  if '--compile_js' in sys.argv:
+    for f in glob.glob('build/scripts/*.js'):
+      print 'Compiling JS:', f
+      data = open(f).read()
+      open(f, 'w').write(compileJS(data))
 
-    raw_input('Done. Press enter to continue.')
+  # Compile HTML.
+  for f in glob.glob('build/*.htm*'):
+    print 'Compiling HTML:', f
+    data = compileHTML(f)
+    open(f, 'w').write(data)
+
+  raw_input('Done. Press enter to continue.')
