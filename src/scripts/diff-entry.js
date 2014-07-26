e05c9e18a9d90eff51500ed6593c37352f039f02
Max Shawabkeh <max99x@users.noreply.github.com>
2014-07-26 20:30:15 +0000

    Show the user a detailed stack trace when diff fails.

--- src/scripts/diff-entry.js
+++ src/scripts/diff-entry.js
@@ -1,5 +1,10 @@
 $(function() {
   $('title').text(chrome.i18n.getMessage('diff_title'));
   $('#diff_in_progress').text(chrome.i18n.getMessage('diff_progress'));
-  initiateDiff(atob(window.location.hash.substr(1)));
+  try {
+    initiateDiff(atob(window.location.hash.substr(1)));
+  } catch (e) {
+    alert(chrome.i18n.getMessage('diff_error') + '\n\n' +
+          e.message + '\n' + e.stack);
+  }
 });
